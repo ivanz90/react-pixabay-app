85db4fc041512b8c97067b5c629744ac9e8acf50
Corvuss90 <ivoron.fantasy@gmail.com>
2020-10-06 16:34:08 +0300

    Radio group typization start

--- src/Components/_ui/RadioGroup/RadioGroup.tsx
+++ src/Components/_ui/RadioGroup/RadioGroup.tsx
@@ -3,7 +3,19 @@ import RadioButton from './RadioButton'
 import { RadioItem } from './style'
 import { FieldTitle } from '../_shared-styles'
 
-const RadioGroup = ({ options, title, register, ...otherProps }) => {
+type option = {
+  id: number
+  value: string
+  label: string
+}
+
+interface IRadioGroup {
+  options: option[];
+  title: string;
+  register?: any;
+}
+
+const RadioGroup: React.FC<IRadioGroup> = ({ options, title, register, ...otherProps }) => {
   return (
     <>
       {!!title && <FieldTitle>{title}</FieldTitle>}
