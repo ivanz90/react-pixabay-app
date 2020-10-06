d1226d8d31985d4b45dbc6eef12206a2209bcba2
Corvuss90 <ivoron.fantasy@gmail.com>
2020-10-06 12:35:28 +0300

    Add TS. TextInput component types

--- src/Components/_ui/TextInput/TextInputView.tsx
+++ src/Components/_ui/TextInput/TextInputView.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { TextInputProps } from './../../../shared/interfaces'
 import { FieldTitle } from '../_shared-styles'
 import { StyledInput } from './style'
 
-const TextInputView = React.forwardRef(({ title, ...restProps }, ref) => {
+const TextInputView = React.forwardRef<HTMLInputElement, TextInputProps>(({ title, ...restProps }, ref) => {
+  console.log({...restProps})
   return ( 
     <label>
       {!!title && <FieldTitle>{title}</FieldTitle>}
--- src/Components/_ui/TextInput/style.ts
+++ src/Components/_ui/TextInput/style.ts
@@ -1,8 +1,8 @@
 import styled from 'styled-components'
+import { TextInputProps } from './../../../shared/interfaces'
 import { INPUT_BORDER_DEFAULT, TEXT_COLOR, INPUT_BORDER_FOCUS } from '../../../shared/style'
 
-
-export const StyledInput = styled.input`
+export const StyledInput = styled.input<TextInputProps>`
   font-size: 16px;
   display: block;
   padding: 10px 20px;
