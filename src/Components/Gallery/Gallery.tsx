3538caeb07af6116a4d9784869be53f396cd90ed
Corvuss90 <ivoron.fantasy@gmail.com>
2020-10-09 14:14:43 +0300

    Gallery component typization

--- src/Components/Gallery/Gallery.tsx
+++ src/Components/Gallery/Gallery.tsx
@@ -12,16 +12,22 @@ const breakpointColumnsObj = {
   500: 1
 }
 
+interface IHit {
+  id: number;
+  pageUrl: string;
+  webformatURL: string;
+}
+
 const Gallery = () => {
-  const hits = useSelector((state) => searchSelectors.selectHits(state), shallowEqual)
+  const hits: IHit[] = useSelector((state) => searchSelectors.selectHits(state), shallowEqual)
 
   return (
     <GallerySection>
       <GalleryWrap>
-        {hits.length === '0' && <p>Nont found</p>}
+        {!hits.length && <p>Nont found</p>}
         {hits.length > 0 && (
           <Masonry breakpointCols={breakpointColumnsObj} gutter={14}>
-            {hits.map((hit) => {
+            {hits.map((hit: IHit) => {
               return <GalleryCard {...hit} key={hit.id} />
             })}
           </Masonry>
