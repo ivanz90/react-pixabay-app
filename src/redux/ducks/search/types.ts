b887eb72a491e01d8a14bf16fd3af2f8739a272b
Ivan Z <corvuss90@gmail.com>
2020-10-22 16:15:07 +0300

    Improve actions and reducer types

--- src/redux/ducks/search/types.ts
+++ src/redux/ducks/search/types.ts
@@ -1,5 +1,8 @@
-import constants from './constants'
 import { IHit } from './../../../shared/types'
+import actions from './actions'
+
+type TInferValue<T> = T extends {[key: string]: infer U} ? U : never
+export type TAction = ReturnType<TInferValue<typeof actions>>
 
 export interface IRootState {
   search: ISearchState
@@ -27,41 +30,3 @@ export interface ISuccess {
   hits: IHit[]
   totalHits: number
 }
-
-export interface ISubmitSuccess {
-  type: typeof constants.SUBMIT_SUCCESS
-  payload: ISuccess
-}
-
-export interface ISubmitError {
-  type: typeof constants.SUBMIT_ERROR
-  payload: string
-}
-
-export interface ISubmitPending {
-  type: typeof constants.SUBMIT_PENDING
-  payload: boolean
-}
-
-export interface ILoadMorePending {
-  type: typeof constants.SUBMIT_PENDING
-  payload: boolean
-}
-
-export interface ISetPage {
-  type: typeof constants.SET_PAGE
-  payload: number
-}
-
-export interface IUpdateHits {
-  type: typeof constants.UPDATE_HITS
-  payload: IHit[]
-}
-
-export type TSearchActions =
-  | ISubmitSuccess
-  | ISubmitError
-  | ISubmitPending
-  | ILoadMorePending
-  | ISetPage
-  | IUpdateHits
